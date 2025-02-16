ee073385686640329f2f219daf23ff8addb8f9de
Suraj15032003 <suraj1412.be21@chitkara.edu.in>
2025-02-16 14:45:28 +0530

    [Suraj]Fix:password polices

--- user.py
+++ user.py
@@ -132,11 +132,6 @@ def valid_password(password):
     """
     Validates a password based on the following predefined rules:
     - Minimum 8 characters.
-    - At least one uppercase letter.
-    - At least one lowercase letter.
-    - At least one digit.
-    - At least one special character (!@#$%^&*).
-
     Parameters:
     password (str): The password entered by the user.
 
@@ -147,7 +142,7 @@ def valid_password(password):
         if not isinstance(password, str):
             raise TypeError("Input must be a string.")
 
-        pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{8,}$'
+        pattern = r'^.{8}$'
         if re.match(pattern, password):
             print("Valid password")
             return True
